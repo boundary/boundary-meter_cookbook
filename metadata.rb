b8cb3718496859baa378e9f4a75df70a34f97ca8
Scott Smith <scott@boundary.com>
2014-04-02 14:31:20 -0700

    fix version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@boundary.com'
 license          'Apache 2.0'
 description      'Installs/Configures bprobe'
 long_description 'Installs/Configures bprobe'
-version          '1.0.0.rc.0'
+version          '0.99.0'
 
 %w{ ubuntu debian rhel centos amazon scientific }.each do |os|
   supports os
