11303bb9fe95caff5ad54e2bf2b145e39ada8416
Zachary Schneider <schneider@boundary.com>
2014-07-14 14:37:27 -0500

    please foodcritic

--- providers/default.rb
+++ providers/default.rb
@@ -24,19 +24,19 @@ include Boundary::Meter
 
 action :create do
   if meter_exists?(new_resource)
-    new_resource.updated_by_last_action false
+    new_resource.updated_by_last_action(false)
   else
     create_meter new_resource 
-    new_resource.updated_by_last_action true
+    new_resource.updated_by_last_action(true)
   end
 end
 
 action :delete do
   if meter_exists?(new_resource)
     delete_meter new_resource 
-    new_resource.updated_by_last_action true
+    new_resource.updated_by_last_action(true)
   else
-    new_resource.updated_by_last_action false
+    new_resource.updated_by_last_action(false)
   end
 end
 
