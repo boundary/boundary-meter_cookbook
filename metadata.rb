49b6c8998b657047715ab84442fb2e748833e4aa
Zachary Schneider <schneider@boundary.com>
2015-06-16 08:18:15 -0500

    bump meta

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@boundary.com'
 license          'Apache 2.0'
 description      'Installs/Configures boundary-meter'
 long_description 'Installs/Configures boundary-meter'
-version          '3.1.5'
+version          '3.1.6'
 
 %w{ ubuntu debian rhel centos amazon scientific }.each do |os|
   supports os
